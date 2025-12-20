ff881fff2b7c5666b9835938ee6d87d38753bc67
AhmedElmasry1985 <30432819+AhmedElmasry1985@users.noreply.github.com>
2025-12-20 11:40:14 +0200

    Create Core library with IRepository and its implementation

---
+++ src/Core/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Core;
+
+public interface IRepository<TEntity> where TEntity : class
+{
+    void Add(TEntity entity);
+    void AddRange(IEnumerable<TEntity> entities);
+    void Remove(TEntity entity);
+    void RemoveRange(IEnumerable<TEntity> entities);
+    TEntity? Find(int id);
+    IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+}
---
+++ src/Core/Repository.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core;
+
+public class Repository<TEntity>:IDisposable,IRepository<TEntity> where TEntity : class
+{
+    private readonly DbContext _context;
+
+    public Repository(DbContext context)
+    {
+        _context = context;
+    }
+    public void Add(TEntity entity)
+    {
+        _context.Set<TEntity>().Add(entity);
+    }
+
+    public void AddRange(IEnumerable<TEntity> entities)
+    {
+        _context.Set<TEntity>().AddRange(entities);
+    }
+
+    public void Remove(TEntity entity)
+    {
+        _context.Set<TEntity>().Remove(entity);
+    }
+
+    public void RemoveRange(IEnumerable<TEntity> entities)
+    {
+        _context.Set<TEntity>().RemoveRange(entities);
+    }
+
+    public TEntity? Find(int id)
+    {
+        return _context.Set<TEntity>().Find(id);
+    }
+
+    public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+    {
+        return _context.Set<TEntity>().Where(predicate);
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+}
