c8bf35f89ed0ff3b51004df406e9aa4ca847ceeb
AhmedElmasry1985 <30432819+AhmedElmasry1985@users.noreply.github.com>
2026-01-04 13:49:45 +0200

    Move Jwt to separate component (Core was depending on ASP.Net Core!), Implement Notification Service

---
+++ src/NotificationService/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NotificationService.Services.MessageBus;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddSingleton<IEventProcessor, EventProcessor>();
+builder.Services.AddHostedService<RabbitMQSubscriber>();
+var app = builder.Build();
+Console.WriteLine("--> NotificationService starting...");
+await app.RunAsync();
